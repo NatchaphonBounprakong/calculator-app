80c001cb01ce200ee19c3d4644108145cdce0961
natchaphon boonprakong <natchaphon2140@gmail.com>
2020-10-24 10:27:22 +0700

    [SSP-24] Initial apps

---
+++ index.js
@@ -0,0 +1,12 @@
+function plus() {
+  var a = document.getElementById("input1").value;
+  var b = document.getElementById("input2").value;
+  var c = document.getElementById("input3");
+  c.value = parseFloat(a) + parseFloat(b);
+}
+
+function minus() {}
+
+function divide() {}
+
+function multiple() {}
