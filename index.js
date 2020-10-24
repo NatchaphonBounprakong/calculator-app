b7a0f93c43e0f4fe9558e0125be107bcab004c1d
natchaphon boonprakong <natchaphon2140@gmail.com>
2020-10-24 10:40:49 +0700

    [SSP-24] Remove unnesscesery file

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function plus() {
-  var a = document.getElementById("input1").value;
-  var b = document.getElementById("input2").value;
-  var c = document.getElementById("input3");
-  c.value = parseFloat(a) + parseFloat(b);
-}
+function plus() {}
 
 function minus() {}
 
